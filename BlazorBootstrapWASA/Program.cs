ba228266705653b37f70a005c83a16ebbbbdb24b
Konrad Wiśniewski <konrad.wisniewski87@gmail.com>
2025-06-24 12:57:56 +0200

    Small refactor

--- BlazorBootstrapWASA/Program.cs
+++ BlazorBootstrapWASA/Program.cs
@@ -1,7 +1,5 @@
-using BlazorBootstrapWASA.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System;
 
 namespace BlazorBootstrapWASA
 {
@@ -12,7 +10,7 @@ namespace BlazorBootstrapWASA
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-            builder.Services.AddSingleton<ThemeProvider>();
+            builder.Services.AddSingleton<BlazorBootstrapWASA.Services.ThemeProvider>();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
